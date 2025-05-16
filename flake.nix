e3694bbaf8f5c47173541b1a434c6f8bd5a64bcf
Najib Ibrahim <mnajib@gmail.com>
2025-05-16 09:39:30 +0800

    install-zfs-nixos now included inside ISO

--- flake.nix
+++ flake.nix
@@ -51,17 +51,34 @@
           statix
           deadnix
 
-          # Helpful tools
+          # Other tools
           git
           jq
+          gnugrep
           vim
           neovim
           htop
           tmux
           dig
+          wget
+          parted
+          coreutils
+          dosfstools # for mkfs.fat
+          zfs # for zpool/zfs
 
           # For testing ISO builds
           #self.packages.${system}.iso
+
+          # Virtualization tools
+          qemu #qemu_kvm
+          virt-manager #
+          libvirt # Virtualization API toolkit
+          edk2 #
+          OVMF # UEFI firmware for VMs
+
+          # KVM utilities
+          virtiofsd # For fast filesystem sharing
+          swtpm # TPM emulation
         ];
 
         shellHook = ''
@@ -70,6 +87,7 @@
           echo ""
           echo "================================================"
           echo "    NixOS Custom ISO Development Environment    "
+          echo "      by NajibMalaysia <mnajib@gmail.com>"
           echo "------------------------------------------------"
 
           echo -e "\033[1;34mAvailable commands:\033[0m"
@@ -91,11 +109,48 @@
           echo "  lint      = statix check . && deadnix ."
           echo "  clean     = rm -f result && echo 'Cleaned!'"
 
+          # Define check-kvm function
+          check-kvm() {
+            echo -e "\n\033[1;36mKVM Status Check:\033[0m"
+            if [ -c /dev/kvm ]; then
+              echo "✓ KVM device found at /dev/kvm"
+              ${nixpkgs.legacyPackages.x86_64-linux.gnugrep}/bin/grep -E '(vmx|svm)' /proc/cpuinfo | \
+                ${nixpkgs.legacyPackages.x86_64-linux.coreutils}/bin/head -n 1 | \
+                ${nixpkgs.legacyPackages.x86_64-linux.coreutils}/bin/sed 's/^/✓ CPU virtualization flags: /'
+            else
+              echo -e "\033[1;31m✗ KVM device not found!\033[0m"
+              echo "  Ensure:"
+              echo "  1. Virtualization enabled in BIOS"
+              echo "  2. Kernel modules loaded (kvm_intel/kvm_amd)"
+              echo "  3. User in kvm group (run: sudo usermod -aG kvm $USER)"
+            fi
+          }
+
+          # Define test-vm function
+          test-vm() {
+            echo -e "\n\033[1;36mStarting Test VM:\033[0m"
+            ${nixpkgs.legacyPackages.x86_64-linux.qemu}/bin/qemu-system-x86_64 \
+              -machine accel=kvm \
+              -cpu host \
+              -m 512 \
+              -nographic \
+              -nodefaults \
+              -serial mon:stdio \
+              -device virtio-net,netdev=user0 \
+              -netdev user,id=user0
+          }
+
           alias build-iso='nix build .#iso'
           alias run-vm="nix run .#vm"
+          alias vm-start="nix run .#vm"
+          alias vm-stop="pkill qemu-system-x86"
+          alias vm-info="virsh list --all"
           alias format="nixpkgs-fmt flake.nix && statix ."
           alias lint="statix check . && deadnix ."
           alias clean="rm -f result && echo 'Cleaned!'"
+          alias check-kvm="check-kvm"
+          alias test-vm="test-vm"
+          alias full-workflow="nix build .#iso && check-kvm && nix run .#vm"
 
           echo "================================================"
           echo ""
@@ -116,9 +171,26 @@
       nixosConfigurations.custom-iso = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+
           #"${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
-          ({ pkgs, ... }: {
+
+          ({ config, pkgs, lib, ... }: {
+
+            imports = [
+              #./users.nix
+            ];
+
+            # ISO image configuration
+            isoImage = {
+              #isoName = lib.mkDefault "najib-nixos-gnome-installer.iso";
+              #isoName = lib.mkForce "nixos-najib-gnome-installer.iso";
+              makeEfiBootable = true;
+              makeUsbBootable = true;
+            };
+
+            networking.hostName = "najib-nixos";
+
             # Custom packages
             environment.systemPackages = with pkgs; [
               tmux
@@ -129,7 +201,7 @@
               git
 
               #nixos-install-tools
-              #gnome-terminal
+              gnome-terminal
               parted
               gparted
               polkit_gnome
@@ -137,6 +209,8 @@
               nixpkgs-fmt
               statix
               deadnix
+
+              self.packages.x86_64-linux.zfs-installer
             ];
 
             # Select internationalisation properties.
@@ -185,7 +259,14 @@
             #  user = "najib";
             #};
 
+            systemd.services."autovt@tty1".enable = false; # Show proper login prompt
+
+            services.udisks2.enable = true;
+            services.gvfs.enable = true;
+            services.accounts-daemon.enable = true;
+
           })
+
         ];
       }; # End nixosConfigurations
       #----------------------------------------------------------------------------
@@ -199,6 +280,65 @@
       #    nix build .#iso
       #
       packages.x86_64-linux.iso = self.nixosConfigurations.custom-iso.config.system.build.isoImage;
+      #----------------------------------------------------------------------------
+
+
+      #----------------------------------------------------------------------------
+      # NixOS installer script
+      #----------------------------------------------------------------------------
+      packages.x86_64-linux.zfs-installer = let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+        pkgs.writeShellScriptBin "install-zfs-nixos" ''
+          set -euxo pipefail
+          ${pkgs.parted}/bin/parted /dev/vda -- mklabel gpt
+          ${pkgs.parted}/bin/parted /dev/vda -- mkpart ESP fat32 1MiB 512MiB
+          ${pkgs.parted}/bin/parted /dev/vda -- set 1 esp on
+          ${pkgs.parted}/bin/parted /dev/vda -- mkpart primary 512MiB 100%
+
+          ${pkgs.parted}/bin/parted /dev/vdb -- mklabel gpt
+          ${pkgs.parted}/bin/parted /dev/vdb -- mkpart ESP fat32 1MiB 512MiB
+          ${pkgs.parted}/bin/parted /dev/vdb -- set 1 esp on
+          ${pkgs.parted}/bin/parted /dev/vdb -- mkpart primary 512MiB 100%
+
+          ${pkgs.dosfstools}/bin/mkfs.fat -F32 /dev/vda1
+          ${pkgs.dosfstools}/bin/mkfs.fat -F32 /dev/vdb1
+
+          ${pkgs.kmod}/bin/modprobe zfs || true
+          ${pkgs.zfs}/bin/zpool create -f -o ashift=12 \
+            -O mountpoint=none \
+            -O compression=lz4 \
+            -O atime=off \
+            zroot mirror /dev/vda2 /dev/vdb2
+
+          ${pkgs.zfs}/bin/zfs create -o mountpoint=legacy zroot/root
+          mount -t zfs zroot/root /mnt
+          mkdir -p /mnt/boot
+          mount /dev/vda1 /mnt/boot
+
+          nixos-generate-config --root /mnt
+          nixos-install --flake ${self}#zfs-vm --no-root-passwd
+        '';
+      #----------------------------------------------------------------------------
+
+
+      #----------------------------------------------------------------------------
+      # App (shortcut) for the NixOS installer script
+      #----------------------------------------------------------------------------
+      #
+      # Become root
+      #   sudo -i
+      #
+      # Run the ZFS installer
+      #   nix run github:your-username/your-repo#zfs-install
+      #
+      # Or if using the local flake:
+      #   /nix/var/nix/profiles/system/activate/install-zfs-nixos
+      #
+      apps.x86_64-linux.zfs-install = {
+        type = "app";
+        program = "${self.packages.x86_64-linux.zfs-installer}/bin/install-zfs-nixos";
+      };
       #----------------------------------------------------------------------------
 
 
@@ -239,8 +379,18 @@
       #
       packages.x86_64-linux.vm = let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        name = "nixos-test-vm";
       in pkgs.writeShellScriptBin "run-vm" ''
         set -e
+        set -euxo pipefail
+
+        TMPDIR=$(mktemp -d)
+        trap "rm -rf $TMPDIR" EXIT
+
+        DISK1="$TMPDIR/disk1.qcow2"
+        DISK2="$TMPDIR/disk2.qcow2"
+
+        # Location for the generated ISO
         ISO_PATH="${self.packages.x86_64-linux.iso}/iso/nixos-*.iso"
         ISO=$(ls $ISO_PATH 2>/dev/null || true)
 
@@ -256,6 +406,10 @@
           exit 1
         fi
 
+        echo "Creating virtual disks..."
+        ${pkgs.qemu}/bin/qemu-img create -f qcow2 "$DISK1" 11G
+        ${pkgs.qemu}/bin/qemu-img create -f qcow2 "$DISK2" 11G
+
         echo "Starting QEMU VM with:"
         echo "  ISO: $ISO"
         echo "Press Ctrl+Alt+G to release mouse/keyboard"
@@ -266,17 +420,28 @@
           -cpu host \
           -smp 2 \
           -m 2048 \
+          -drive file=$TMPDIR/disk1.qcow2,if=virtio,format=qcow2 \
+          -drive file=$TMPDIR/disk2.qcow2,if=virtio,format=qcow2 \
           -cdrom "$ISO" \
           -boot d \
-          -audiodev pa,id=snd0 \
-          -device AC97,audiodev=snd0 \
           -nic user,model=virtio-net-pci \
           -usb \
           -device usb-tablet \
           -name "NixOS-Custom-ISO"
       '';
+        #${pkgs.qemu}/bin/qemu-system-x86_64 \
+        #${pkgs.qemu_kvm}/bin/qemu-system-x86_64 \
+          #-bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          #-drive file=$TMPDIR/nixos.iso,media=cdrom \
+          #-drive file=$TMPDIR/disk1.qcow2,if=virtio,format=qcow2 \
+          #-drive file=$TMPDIR/disk2.qcow2,if=virtio,format=qcow2 \
+          #-netdev user,id=net0,hostfwd=tcp::2222-:22 \
+          #-device virtio-net,netdev=net0 \
+          #-enable-kvm \
           #-display gtk,gl=on \
           #-device virtio-vga-gl \
+          #-audiodev pa,id=snd0 \
+          #-device AC97,audiodev=snd0 \
       # To make RAM 4GB: '-m 4096'
       # To disable GUI: replace '-display gtk' with '-nographic'
       # To disable audio: delete '-audiodev' and '-device AC97' lines
